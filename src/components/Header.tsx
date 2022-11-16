0d40837cb44d975b32e9818a4003b30008ae070f
Ournopshhpshh <antonnovikovdev@gmail.com>
2022-11-16 21:50:40 +0300

    good

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header(){
+    return(
+        <div className={"header"}><header>
+            <img src="https://miro.medium.com/max/720/1*p8SGPFKYcbRWKfdj50wNxQ.png" alt=""/>
+        </header></div>
+    )
+}
