07138db8652f527a10be15d33d9a607d6651fed9
Ournopshhpshh <antonnovikovdev@gmail.com>
2022-11-20 00:10:25 +0300

    21 урок+разобрался с Роутингом

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import s from "./Dialogs.module.css"
+
 
 
 export function Dialogs() {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Anton
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>1</div>
+                <div className={s.message}>2</div>
+                <div className={s.message}>3</div>
+                <div className={s.message}>4</div>
+                <div className={s.message}>5</div>
+            </div>
+        </div>
     )
 
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,21 +3,20 @@ import s from "./NavBar.module.css"
 import {Link, NavLink} from "react-router-dom";
 export const NavBar = () => {
     let setActive=({isActive}:{isActive:boolean}) => (isActive ? s.active : s.nonactive)
-
     return (
         <div className={s.navbar}>
             <nav>
                 <div className={s.item}>
                     <div><NavLink to={"/Profile"}
-                                  className={({isActive}) => isActive ? s.active : s.nonactive}>Profile</NavLink></div>
+                                  className={setActive}>Profile</NavLink></div>
                     <div><NavLink to={"/Dialogs"}
-                                  className={({isActive}) => isActive ? s.active : s.nonactive}>Dialogs</NavLink></div>
+                                  className={setActive}>Dialogs</NavLink></div>
                     <div><NavLink to={"/News"}
-                                  className={({isActive}) => isActive ? s.active : s.nonactive}>News</NavLink></div>
+                                  className={setActive}>News</NavLink></div>
                     <div><NavLink to={"/Music"}
-                                  className={({isActive}) => isActive ? s.active : s.nonactive}>Music</NavLink></div>
+                                  className={setActive}>Music</NavLink></div>
                     <div><NavLink to={"/Settings"}
-                                  className={({isActive}) => isActive ? s.active : s.nonactive}>Settings</NavLink></div>
+                                  className={setActive}>Settings</NavLink></div>
                 </div>
                 {/*<div className={stylesNavBar.item}><a>Profile</a></div>*/}
                 {/*<div className={stylesNavBar.item}><a>Messages</a></div>*/}
